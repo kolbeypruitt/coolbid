41282ad4655c57784c55dee77a84ff0a8b9c96d7
Kolbey Pruitt <kolbeypruitt@gmail.com>
2026-04-09 08:13:35 -0500

    feat: add ambient trial banner to app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
+import { TrialBanner } from "@/components/billing/trial-banner";
 
 export default async function AppLayout({
   children,
@@ -22,6 +23,7 @@ export default async function AppLayout({
       <Sidebar />
       <div className="flex flex-1 flex-col">
         <Header email={user.email ?? ""} />
+        <TrialBanner />
         <main className="flex-1 overflow-y-auto p-6">{children}</main>
       </div>
     </div>
